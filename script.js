824532fd3e0df240350f84859cedace6e15548fe
Ankita Das <daszankita23@gmail.com>
2025-07-24 23:52:05 +0530

    stories can be seen

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const stories=document.querySelector(".stories");
+const screen=document.querySelector(".fullscreen");
 const arr = [
   {
     dp: "https://images.unsplash.com/photo-1516195851888-6f1a981a862e?q=80&w=705&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
@@ -31,8 +32,9 @@ arr.forEach(function(elem,idx){
 
 stories.innerHTML=count;
 
-stories.addEventListener("click",function(){
-    
+stories.addEventListener("click",function(dets){
+    screen.style.display="block";
+    screen.style.backgroundImage=`url(${arr[dets.target.id].story})`;
 })
 
 

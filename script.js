26aaf80396e0e956cad70088bcb9ea32ad5920b6
Ankita Das <daszankita23@gmail.com>
2025-07-24 23:43:58 +0530

    screen added for stories

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const stories=document.querySelector(".stories");
 const arr = [
   {
     dp: "https://images.unsplash.com/photo-1516195851888-6f1a981a862e?q=80&w=705&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
@@ -21,12 +22,17 @@ const arr = [
   }
 ];
 let count="";
-arr.forEach(function(elem){
+arr.forEach(function(elem,idx){
     count+=`<div class="story">
           <img
-            src="${elem.dp}"
-            alt="" />
+          id=${idx} src="${elem.dp}" alt="" />
           </div>`;
 })
 
-document.querySelector(".stories").innerHTML=count;
+stories.innerHTML=count;
+
+stories.addEventListener("click",function(){
+    
+})
+
+
